066356af33e68bb9b3f8332c0c33dea4ba0c7bf7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-03-30 10:14:12 -0700

    added module description for overview

--- bin/install.sh
+++ bin/install.sh
@@ -7,7 +7,8 @@ sudo apt-get install -y git realpath pandoc
 sudo deluser jns
 echo | sudo adduser --disabled-login jns
 
-sudo usermod -aG sudo,ssh jns
+# jns should be no sudoer nor ssh able
+# sudo usermod -aG sudo,ssh jns
 
 echo "y" | sudo -H -u jns ./jns-install.sh
 
